a3193a79af40f5a3e90c59712fcc43b4d30306a7
Aaron A. King <kingaa@umich.edu>
2007-07-22 15:35:22 +0000

    pass arguments down to pfilter.pomp explicitly (rather than through ...)
    git-svn-id: svn+ssh://salix/svn/king/pomp/trunk@12 e6a46c30-f711-4166-b69d-97087421640b

--- R/pfilter-mif.R
+++ R/pfilter-mif.R
@@ -1,10 +1,21 @@
-pfilter.mif <- function (object,
-                         Np = object@alg.pars$Np,
-                         coef = object@coef,
-                         ...
-                         ) {
+pfilter.mif <- function (object, Np, coef, tol = 1e-17, warn = TRUE, max.fail = 0,
+                         pred.mean = FALSE, pred.var = FALSE, filter.mean = FALSE, ...) {
+  if (missing(Np))
+    Np <- object@alg.pars$Np
+  if (missing(coef))
+    coef <- object@coef
   x <- particles(object,Np=Np,center=coef,sd=0)
-  pfilter(as(object,'pomp'),xstart=x$states,params=x$params,...)
+  pfilter(
+          as(object,'pomp'),
+          xstart=x$states,
+          params=x$params,
+          tol=tol,
+          warn=warn,
+          max.fail=max.fail,
+          pred.mean=pred.mean,
+          pred.var=pred.var,
+          filter.mean=filter.mean,
+          ...)
 }
 
 setMethod('pfilter','mif',pfilter.mif)
