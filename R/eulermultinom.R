4fc7c7bbee7277174044f08715834feb6a6a8852
Aaron A. King <kingaa@umich.edu>
2008-05-13 15:12:38 +0000

    use .Call interface
    git-svn-id: svn+ssh://salix/svn/king/pomp/trunk@57 e6a46c30-f711-4166-b69d-97087421640b

--- R/eulermultinom.R
+++ R/eulermultinom.R
@@ -1,49 +1,5 @@
-reulermultinom <- function (n = 1, size, rate, dt) {
-  ntrans <- length(rate)
-  if (length(size)>1)
-    warning("only the first element of 'size' is meansingful")
-  size <- floor(size)
-  x <- .C(
-          "reulermultinom_multi",
-          n = as.integer(n),
-          ntrans=as.integer(ntrans),
-          size=as.double(size),
-          rate=as.double(rate),
-          dt=as.double(dt),
-          trans=double(n*ntrans),
-          NAOK=FALSE,
-          DUP=FALSE,
-          PACKAGE="pomp"
-          )$trans
-  dim(x) <- c(ntrans,n)
-  rownames(x) <- names(rate)
-  x
-}
+reulermultinom <- function (n = 1, size, rate, dt)
+  .Call(R_Euler_Multinom,n,size,rate,dt)
 
-deulermultinom <- function (x, size, rate, dt, log = FALSE) {
-  ntrans <- length(rate)
-  if (NROW(x)!=ntrans)
-    stop("deulermultinom: length of 'x' should match length of 'rate'")
-  n <- NCOL(x)
-  if (length(size)>1)
-    warning("only the first element of 'size' is meaningful")
-  if (length(dt)>1)
-    warning("only the first element of 'dt' is meaningful")
-  size <- floor(size[1])
-  f <- .C(
-          "deulermultinom_multi",
-          n=as.integer(n),
-          ntrans=as.integer(ntrans),
-          size=as.double(size),
-          rate=as.double(rate),
-          trans=as.double(x),
-          dt=as.double(dt),
-          give_log=as.integer(log),
-          f=double(n),
-          NAOK=FALSE,
-          DUP=FALSE,
-          PACKAGE="pomp"
-          )$f
-  f
-}
-
+deulermultinom <- function (x, size, rate, dt, log = FALSE)
+  .Call(D_Euler_Multinom,as.matrix(x),size,rate,dt,log)
