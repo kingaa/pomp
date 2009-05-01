bf59f7f0079acbc31302d61aeb6f54beb181abcd
Aaron A. King <kingaa@umich.edu>
2009-05-01 11:29:30 +0000

    when params is missing, use params <- coef(object)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/init.state-pomp.R
+++ pkg/R/init.state-pomp.R
@@ -3,8 +3,8 @@ setMethod(
           'init.state',
           'pomp',
           function (object, params, t0, ...) { # the package algorithms will only use these arguments
-            if (missing(t0))
-              t0 <- object@t0
+            if (missing(t0)) t0 <- object@t0
+            if (missing(params)) params <- coef(object)
             x <- try(
                      .Call(do_init_state,object,as.matrix(params),t0),
                      silent=FALSE
