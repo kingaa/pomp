0f689df70193be617df9a6821ae5d45e74f6955c
Aaron A. King <kingaa@umich.edu>
2010-05-17 12:54:41 +0000

    - fix R-forge logo - update reference list
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@238 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,5 +1,4 @@
 <?php
-
 $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
 $group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
 $themeroot='http://r-forge.r-project.org/themes/rforge/';
@@ -23,7 +22,7 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <table border="0" width="80%" cellspacing="0" cellpadding="0">
 <tr>
 <td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> 
+<a href="/"><img src="<?php echo $themeroot; ?>/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> 
 </td> 
 </tr>
 </table>
