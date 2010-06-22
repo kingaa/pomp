62985246cae79bc6af577a981741c0720ddaf7c1
Aaron A. King <kingaa@umich.edu>
2010-06-22 13:02:39 +0000

    - simplify
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@269 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/bsplines.R
+++ pkg/R/bsplines.R
@@ -1,7 +1,5 @@
-bspline.basis <- function (x, nbasis, degree = 3) {
+bspline.basis <- function (x, nbasis, degree = 3)
   .Call(bspline_basis,x,nbasis,degree)
-}
 
-periodic.bspline.basis <- function (x, nbasis, degree = 3, period = 1) {
+periodic.bspline.basis <- function (x, nbasis, degree = 3, period = 1)
   .Call(periodic_bspline_basis,x,nbasis,degree,period)
-}
