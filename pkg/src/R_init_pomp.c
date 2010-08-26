1d32a55d05085736df62842734b268e7aa886696
Aaron A. King <kingaa@umich.edu>
2010-08-26 20:06:50 +0000

    - register C-callable routines
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@307 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/src/R_init_pomp.c
@@ -0,0 +1,10 @@
+#include "pomp.h"
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+
+void R_init_pomp (DllInfo *info) {
+  R_RegisterCCallable("pomp","periodic_bspline_basis_eval",(DL_FUNC) &periodic_bspline_basis_eval);
+  R_RegisterCCallable("pomp","dot_product",(DL_FUNC) &dot_product);
+  R_RegisterCCallable("pomp","reulermultinom",(DL_FUNC) &reulermultinom);
+  R_RegisterCCallable("pomp","deulermultinom",(DL_FUNC) &deulermultinom);
+}
