419fb202f12cc1e158d93dd5bd58c7fde9b98bad
Aaron A. King <kingaa@umich.edu>
2010-10-04 19:53:57 +0000

    - put guts of trajectory calculation into the named function 'trajectory.internal'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@360 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/trajectory-pomp.R
+++ pkg/R/trajectory-pomp.R
@@ -1,97 +1,95 @@
-trajectory <- function (object, params, times, ...)
+trajectory <- function (object, params, times, t0, ...)
   stop("function ",sQuote("trajectory")," is undefined for objects of class ",sQuote(class(object)))
 setGeneric('trajectory')
 
-setMethod(
-          "trajectory",
-          "pomp",
-          function (object, params, times, ...) {
-            if (missing(params)) {
-              params <- coef(object)
-              if (length(params)==0) {
-                stop("trajectory error: ",sQuote("params")," must be supplied",call.=FALSE)
-              }
-            }
-            nrep <- NCOL(params)
-            if (is.null(dim(params))) {
-              params <- matrix(
-                               params,
-                               nrow=length(params),
-                               ncol=nrep,
-                               dimnames=list(
-                                 names(params),
-                                 NULL
-                                 )
-                               )
-            }
-            paramnames <- rownames(params)
-            if (is.null(paramnames))
-              stop("pfilter error: ",sQuote("params")," must have rownames",call.=FALSE)
-            params <- as.matrix(params)
-
-            if (missing(times))
-              times <- time(object,t0=TRUE)
-            else
-              times <- as.numeric(times)
-
-            tm <- times[1]
-            x0 <- init.state(object,params=params,t0=tm)
-            nm <- rownames(x0)
-            dim(x0) <- c(nrow(x0),nrep,1)
-            dimnames(x0) <- list(nm,NULL,NULL)
-            
-            x <- array(
-                       dim=c(nrow(x0),nrep,length(times)),
-                       dimnames=list(rownames(x0),NULL,NULL)
+trajectory.internal <- function (object, params, times, t0, ...) {
+  if (missing(params)) {
+    params <- coef(object)
+    if (length(params)==0) {
+      stop("trajectory error: ",sQuote("params")," must be supplied",call.=FALSE)
+    }
+  }
+  nrep <- NCOL(params)
+  if (is.null(dim(params))) {
+    params <- matrix(
+                     params,
+                     nrow=length(params),
+                     ncol=nrep,
+                     dimnames=list(
+                       names(params),
+                       NULL
                        )
-            switch(
-                   object@skeleton.type,
-                   map={                # iterate the map
-                     for (k in seq_along(times)) {
-                       if (tm < times[k]) {
-                         while (tm < times[k]) {
-                           x0[,,] <- skeleton(object,x=x0,t=tm,params=params)
-                           tm <- tm+1
-                         }
-                       }
-                       x[,,k] <- x0
-                     }
-                   },
-                   vectorfield={        # integrate the vectorfield
-                     for (j in seq_len(nrep)) {
-                       X <- try(
-                                deSolve::lsoda(
-                                               y=x0[,j,1],
-                                               times=times,
-                                               func=function(t,y,parms){
-                                                 list(
-                                                      skeleton(
-                                                               object,
-                                                               x=array(
-                                                                 data=y,
-                                                                 dim=c(length(y),1,1),
-                                                                 dimnames=list(names(y),NULL,NULL)
-                                                                 ),
-                                                               t=t,
-                                                               params=as.matrix(parms)
-                                                               ),
-                                                      NULL
-                                                      )
-                                               },
-                                               parms=params[,j],
-                                               ...
-                                               ),
-                                silent=FALSE
-                                )
-                       if (inherits(X,'try-error'))
-                         stop("trajectory error: error in ",sQuote("lsoda"),call.=FALSE)
-                       if (attr(X,'istate')[[1]]!=2)
-                         warning("abnormal exit from ",sQuote("lsoda"),", istate = ",attr(X,'istate'),call.=FALSE)
-                       x[,j,] <- t(X[,-1])
-                     }
-                   },
-                   unspecified=stop("deterministic skeleton not specified")
-                   )
-            x
-          }
-          )
+                     )
+  }
+  paramnames <- rownames(params)
+  if (is.null(paramnames))
+    stop("pfilter error: ",sQuote("params")," must have rownames",call.=FALSE)
+  params <- as.matrix(params)
+
+  if (missing(times))
+    times <- time(object,t0=TRUE)
+  else
+    times <- as.numeric(times)
+
+  tm <- times[1]
+  x0 <- init.state(object,params=params,t0=tm)
+  nm <- rownames(x0)
+  dim(x0) <- c(nrow(x0),nrep,1)
+  dimnames(x0) <- list(nm,NULL,NULL)
+  
+  x <- array(
+             dim=c(nrow(x0),nrep,length(times)),
+             dimnames=list(rownames(x0),NULL,NULL)
+             )
+  switch(
+         object@skeleton.type,
+         map={                # iterate the map
+           for (k in seq_along(times)) {
+             if (tm < times[k]) {
+               while (tm < times[k]) {
+                 x0[,,] <- skeleton(object,x=x0,t=tm,params=params)
+                 tm <- tm+1
+               }
+             }
+             x[,,k] <- x0
+           }
+         },
+         vectorfield={        # integrate the vectorfield
+           for (j in seq_len(nrep)) {
+             X <- try(
+                      deSolve::lsoda(
+                                     y=x0[,j,1],
+                                     times=times,
+                                     func=function(t,y,parms){
+                                       list(
+                                            skeleton(
+                                                     object,
+                                                     x=array(
+                                                       data=y,
+                                                       dim=c(length(y),1,1),
+                                                       dimnames=list(names(y),NULL,NULL)
+                                                       ),
+                                                     t=t,
+                                                     params=as.matrix(parms)
+                                                     ),
+                                            NULL
+                                            )
+                                     },
+                                     parms=params[,j],
+                                     ...
+                                     ),
+                      silent=FALSE
+                      )
+             if (inherits(X,'try-error'))
+               stop("trajectory error: error in ",sQuote("lsoda"),call.=FALSE)
+             if (attr(X,'istate')[[1]]!=2)
+               warning("abnormal exit from ",sQuote("lsoda"),", istate = ",attr(X,'istate'),call.=FALSE)
+             x[,j,] <- t(X[,-1])
+           }
+         },
+         unspecified=stop("deterministic skeleton not specified")
+         )
+  x
+}
+
+setMethod("trajectory",signature=signature(object="pomp"),definition=trajectory.internal)
