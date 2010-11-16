91967731591151e5a2bda3322f2e67641f222248
Aaron A. King <kingaa@umich.edu>
2010-11-16 14:26:16 +0000

    - remove one layer of try/catch
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@419 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/skeleton-pomp.R
+++ pkg/R/skeleton-pomp.R
@@ -1,18 +1,9 @@
-skeleton <- function (object, x, t, params, ...)
-  stop("function ",sQuote("skeleton")," is undefined for objects of class ",sQuote(class(object)))
-setGeneric('skeleton')  
+setGeneric("skeleton",function(object,...)standardGeneric("skeleton"))
 
 ## evaluate the measurement model density function
 setMethod(
           'skeleton',
           'pomp',
-          function (object, x, t, params, ...) {
-            x <- try(
-                     .Call(do_skeleton,object,x,t,params),
-                     silent=FALSE
-                     )
-            if (inherits(x,'try-error'))
-              stop("skeleton error: error in user ",sQuote("skeleton"),call.=FALSE)
-            x
-          }
+          function (object, x, t, params, ...)
+            .Call(do_skeleton,object,x,t,params),
           )
