5e3c44d26f9d9091f5f40bcaab6944f9a49fd273
Aaron A. King <kingaa@umich.edu>
2010-11-29 15:10:51 +0000

    - turn banner back on
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@430 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aaa.R
+++ pkg/R/aaa.R
@@ -1,10 +1,9 @@
-##.onAttach <- function (...) {
-##   version <- library(help=pomp)$info[[1]]
-##   version <- strsplit(version[pmatch("Version",version)]," ")[[1]]
-##   version <- version[nchar(version)>0][2]
-##   cat("This is pomp version ",version,"\n\n",sep="")
-##   cat("See the NEWS file for important information\n")
-##}
+.onAttach <- function (...) {
+  version <- library(help=pomp)$info[[1]]
+  version <- strsplit(version[pmatch("Version",version)]," ")[[1]]
+  version <- version[nchar(version)>0][2]
+  cat("This is pomp version ",version,"\n\n",sep="")
+}
 
 setGeneric("print",function(x,...)standardGeneric("print"))
 setGeneric("plot",function(x,y,...)standardGeneric("plot"))
