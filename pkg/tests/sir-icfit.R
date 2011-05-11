44921bf6e3d83c53cdbe6b24f8fb0afe1f344ec4
Aaron A. King <kingaa@umich.edu>
2011-05-11 21:48:20 +0000

    - add a test for the new 'pfilter'-based initial-condition-estimation method
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@474 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/sir-icfit.R
+++ pkg/tests/sir-icfit.R
@@ -5,10 +5,10 @@ set.seed(343435488L)
 pdf(file="sir-icfit.pdf")
 
 data(euler.sir)
-po <- window(euler.sir,end=0.25)
+po <- window(euler.sir,end=0.2)
 guess <- coef(po)
 ics <- c("S.0","I.0","R.0") 
-guess[ics[-3]] <- guess[ics[-3]]+c(0.5,-0.3)
+guess[ics[-3]] <- guess[ics[-3]]+c(0.2,-0.2)
 
 plist <- list(
               probe.marginal("reports",ref=obs(po),order=3,diff=1,transform=sqrt),
@@ -55,13 +55,7 @@ x <- sapply(
 plot(range(time(po)),range(c(states(po,"cases"),x)),bty='l',xlab="time",ylab="cases",type='n')
 points(time(po),states(po,"cases"))
 matlines(time(po),x,lty=1,col=c("red","blue","green"))
-legend("topright",lty=1,bty='n',col=c("red","blue","green"),legend=colnames(x))
-
-data(euler.sir)
-po <- window(euler.sir,end=0.25)
-guess <- coef(po)
-ics <- c("S.0","I.0","R.0") 
-guess[ics[-3]] <- guess[ics[-3]]+c(0.1,-0.2)
+legend("topright",lty=c(NA,1,1,1),pch=c(1,NA,NA,NA),bty='n',col=c("black","red","blue","green"),legend=c("actual",colnames(x)))
 
 summary(tm.true <- traj.match(po,eval.only=TRUE))
 
@@ -98,6 +92,55 @@ comp.table
 
 x <- sapply(
             list(true=tm.true,guess=tm.guess,fit=tm.fit),
+            function (x) trajectory(x,times=time(x),t0=timezero(x))["cases",1,]
+            )
+
+plot(range(time(po)),range(c(states(po,"cases"),x)),bty='l',xlab="time",ylab="cases",type='n')
+points(time(po),states(po,"cases"))
+matlines(time(po),x,lty=1,col=c("red","blue","green"))
+legend("topright",lty=c(NA,1,1,1),pch=c(1,NA,NA,NA),bty='n',col=c("black","red","blue","green"),legend=c("actual",colnames(x)))
+
+### now try an initial condition fitting approach based on particle filtering
+est <- ics[-1]
+np <- 10000                              # number of particles to use
+pp <- array(coef(po),dim=c(length(coef(po)),np),dimnames=list(names(coef(po)),NULL))
+## generate an array of guesses
+guesses <- sobol.design(lower=guess[est]-0.5,upper=guess[est]+0.5,nseq=np)
+nd <- length(time(po))
+
+## fit the initial conditions using repeated filtering on the initial window of the data
+
+for (j in seq_len(3)) {
+  for (k in est) {
+    pp[k,] <- guesses[[k]]
+  }
+  for (k in seq_len(5)) {
+    pf <- pfilter(po,params=pp,save.params=TRUE)
+    pp <- pf@saved.params[,,nd]
+  }
+  guesses <- sobol.design(
+                          lower=apply(pp[est,],1,min),
+                          upper=apply(pp[est,],1,max),
+                          nseq=np
+                          )
+}
+
+pf.fit <- po
+coef(pf.fit,ics) <- log(apply(apply(exp(pp[ics,]),2,function(x)x/sum(x)),1,mean))
+pf.true <- pfilter(po,Np=2000)
+pf.guess <- pfilter(po,params=guess,Np=2000,max.fail=100)
+pf.fit <- pfilter(pf.fit,Np=2000)
+
+comp.table <- cbind(true=exp(coef(po,ics)),guess=exp(guess[ics]),fit=exp(coef(pf.fit,ics)))
+comp.table <- apply(comp.table,2,function(x)x/sum(x))
+comp.table <- rbind(
+                    comp.table,
+                    loglik=sapply(list(pf.true,pf.guess,pf.fit),logLik)
+                    )
+comp.table
+
+x <- sapply(
+            list(true=pf.true,guess=pf.guess,fit=pf.fit),
             function (x) trajectory(x,times=time(x),t0=timezero(x))["cases",1,]
             )
 
