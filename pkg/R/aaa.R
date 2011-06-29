8ea02048fb5a6e35899b397c8c0a6130e9411b10
Aaron A. King <kingaa@umich.edu>
2011-06-29 18:56:18 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@517 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/aaa.R
+++ pkg/R/aaa.R
@@ -1,10 +1,9 @@
-##.onAttach <- function (...) {
+## .onAttach <- function (...) {
 ##   version <- library(help=pomp)$info[[1]]
 ##   version <- strsplit(version[pmatch("Version",version)]," ")[[1]]
 ##   version <- version[nchar(version)>0][2]
-##   cat("This is pomp version ",version,"\n\n",sep="")
-##   cat("See the NEWS file for important information\n")
-##}
+##   packageStartupMessage("This is pomp version ",version,"\n")
+## }
 
 setGeneric("print",function(x,...)standardGeneric("print"))
 setGeneric("plot",function(x,y,...)standardGeneric("plot"))
