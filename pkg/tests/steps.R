9c9d34074032f52d69e146e370e32af4e4855325
Aaron A. King <kingaa@umich.edu>
2012-01-11 13:12:24 +0000

    - version 0.40-2
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@590 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/tests/steps.R
@@ -0,0 +1,18 @@
+library(pomp)
+
+set.seed(54588699L)
+
+data(ricker)
+coef(ricker,"log.sigma") <- -Inf
+tm <- sort(runif(n=20,max=3))
+x <- trajectory(ricker,times=tm)["N",,]
+y <- simulate(ricker,times=tm,states=TRUE)["N",,]
+stopifnot(identical(x,y))
+
+data(verhulst)
+coef(verhulst,c("n.0","sigma")) <- c(15,0)
+tm <- sort(runif(n=100,max=1))
+x <- trajectory(verhulst,times=tm)["n",,]
+y <- simulate(verhulst,times=tm,states=TRUE)["n",,]
+table(cut(x-y,breaks=10))
+
