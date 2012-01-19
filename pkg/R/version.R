384e9287befd373cff0a5bef3fd2dbb5de1b651f
Aaron A. King <kingaa@umich.edu>
2012-01-19 21:19:22 +0000

    - add a 'version.string' to the return list
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@605 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/version.R
+++ pkg/R/version.R
@@ -2,14 +2,14 @@ version <- function (at.least = NULL) {
   version <- library(help=pomp)$info[[1]]
   version <- strsplit(version[pmatch("Version",version)]," ")[[1]]
   version <- version[nchar(version)>0][2]
-  version <- as.numeric(strsplit(version,"[-.]")[[1]])
+  splv <- as.numeric(strsplit(version,"[-.]")[[1]])
   if (is.null(at.least)) {
-    list(major=version[1],minor=version[2],rev=version[3])
+    list(major=splv[1],minor=splv[2],rev=splv[3],version.string=version)
   } else {
     minv <- as.numeric(strsplit(as.character(at.least),"[-.]")[[1]])
-    (version[1]>minv[1]) ||
-    (version[1]==minv[1]) && (version[2]>minv[2]) ||
-    (version[1]==minv[1]) && (version[2]==minv[2]) && (version[3]>=minv[3])
+    (splv[1]>minv[1]) ||
+    (splv[1]==minv[1]) && (splv[2]>minv[2]) ||
+    (splv[1]==minv[1]) && (splv[2]==minv[2]) && (splv[3]>=minv[3])
   }
 }
 
