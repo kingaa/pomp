403b1003cd169a3ec4cca51ea48ff7a84e233773
Aaron A. King <kingaa@umich.edu>
2012-05-03 19:52:05 +0000

    - forgot to export 'get_pomp_userdata_int' and 'get_pomp_userdata_double'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@704 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/src/R_init_pomp.c
+++ pkg/pomp/src/R_init_pomp.c
@@ -8,4 +8,6 @@ void R_init_pomp (DllInfo *info) {
   R_RegisterCCallable("pomp","reulermultinom",(DL_FUNC) &reulermultinom);
   R_RegisterCCallable("pomp","deulermultinom",(DL_FUNC) &deulermultinom);
   R_RegisterCCallable("pomp","get_pomp_userdata",(DL_FUNC) &get_pomp_userdata);
+  R_RegisterCCallable("pomp","get_pomp_userdata_int",(DL_FUNC) &get_pomp_userdata_int);
+  R_RegisterCCallable("pomp","get_pomp_userdata_double",(DL_FUNC) &get_pomp_userdata_double);
 }
