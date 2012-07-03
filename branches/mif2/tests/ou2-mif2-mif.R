e5ab9dbed567b6faa9f33ab5936f4077019e3eb5
Dao Xuan Nguyen <nguyenxd@umich.edu>
2012-07-03 17:50:53 +0000

    test of mif2 and mif
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@749 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ branches/mif2/tests/ou2-mif2-mif.R
@@ -0,0 +1,47 @@
+library(pomp)
+
+data(ou2)
+
+
+pdf(file="ou2-mif2-mif.pdf")
+
+p.truth <- coef(ou2)
+guess2 <- guess1 <- p.truth
+guess1[c('x1.0','x2.0','alpha.2','alpha.3')] <- 0.9*guess1[c('x1.0','x2.0','alpha.2','alpha.3')]
+guess2[c('x1.0','x2.0','alpha.2','alpha.3')] <- 1.2*guess1[c('x1.0','x2.0','alpha.2','alpha.3')]
+
+set.seed(64857673L)
+mif1 <- mif(ou2,Nmif=100,start=guess1,
+		pars=c('alpha.2','alpha.3'),ivps=c('x1.0','x2.0'),
+		rw.sd=c(
+				x1.0=.5,x2.0=.5,
+				alpha.2=0.1,alpha.3=0.1),
+		transform=F,
+		Np=1000,
+		var.factor=1,
+		cooling.scalar=400,
+		ic.lag=10,
+		cooling.factor=0.95,
+		option="mif2",
+		.ndone=0
+		
+)
+set.seed(64857673L)
+mif2 <- mif(ou2,Nmif=100,start=guess1,
+		pars=c('alpha.2','alpha.3'),ivps=c('x1.0','x2.0'),
+		rw.sd=c(
+				x1.0=0.5,x2.0=.5,
+				alpha.2=0.1,alpha.3=0.1),
+		transform=F,
+		Np=1000,
+		var.factor=1,
+		ic.lag=10,
+		cooling.factor=0.95,
+		max.fail=100,
+		option="mif",
+		.ndone=0
+		
+)  
+compare.mif(list(mif1,mif2))
+
+dev.off()
