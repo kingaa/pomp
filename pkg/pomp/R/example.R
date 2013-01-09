9a2930ed8edd5e53aa3c566ba80de4d4135c09ef
Aaron A. King <kingaa@umich.edu>
2013-01-09 22:59:28 +0000

    - improved 'pompExample'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@805 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/R/example.R
+++ pkg/pomp/R/example.R
@@ -7,7 +7,8 @@ pompExample <- function (example) {
     avlbl <- gsub("\\.R$","",avlbl)
     cat("available pomp examples:\n",sQuote(avlbl),"\n")
   } else {
-    file <- system.file(file.path("examples",paste(example,".R",sep="")),package="pomp")
+    ex <- as.character(substitute(example))
+    file <- system.file(file.path("examples",paste(ex,".R",sep="")),package="pomp")
     objs <- source(file,local=TRUE)
     cat("newly created pomp objects:\n",objs$value,"\n")
   }
