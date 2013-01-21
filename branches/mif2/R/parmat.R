1ecd970df06f69604f2855f8d862fdc0db214e6a
Aaron A. King <kingaa@umich.edu>
2013-01-21 23:09:01 +0000

    - re-branch mif2 from pomp revision 819
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@820 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- branches/mif2/R/parmat.R
+++
@@ -1,6 +0,0 @@
-parmat <- function (params, nrep = 1) {
-  params <- as.matrix(params)
-  p <- matrix(data=params,nrow=nrow(params),ncol=ncol(params)*nrep)
-  rownames(p) <- rownames(params)
-  p
-}
--- branches/mif2/R/skeleton-pomp.R
+++
@@ -1,9 +0,0 @@
-## evaluate the deterministic skeleton
-setGeneric("skeleton",function(object,...)standardGeneric("skeleton"))
-
-skeleton.internal <- function (object, x, t, params, ...) {
-  skel <- .Call(get_pomp_fun,object@skeleton)
-  .Call(do_skeleton,object,x,t,params,skel)
-}
-
-setMethod("skeleton","pomp",skeleton.internal)
