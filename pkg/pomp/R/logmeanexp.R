a5df1e63c807b597ff64fb3cf0dfd6be3d929fc2
Aaron A. King <kingaa@umich.edu>
2014-01-10 19:50:48 +0000

    - add 'se' argument to 'logmeanexp'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@878 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/R/logmeanexp.R
+++ pkg/pomp/R/logmeanexp.R
@@ -1,4 +1,10 @@
-logmeanexp <- function (x) {
+logmeanexp <- function (x, se = FALSE) {
   mx <- max(x)
-  mx+log(mean(exp(x-mx)))
+  mean <- mx+log(mean(exp(x-mx)))
+  if (se) {
+    se <- sd(exp(x-mx))/exp(mean-mx)
+    c(mean,se=se)
+  } else {
+    mean
+  }
 }
