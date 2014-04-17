2f74819ee0b4943f58414b9c8f81e8991ffb385d
Aaron A. King <kingaa@umich.edu>
2014-04-17 13:13:21 +0000

    - tweak ou2-nlf test - fix error in pomp documentation
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@921 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/tests/ou2-nlf.R
+++ pkg/pomp/tests/ou2-nlf.R
@@ -1,6 +1,6 @@
 library(pomp)
 
-set.seed(594861940L)
+set.seed(583615606L)
 
 pompExample(ou2)
 estnames=c("alpha.2","alpha.3","tau")
@@ -17,7 +17,7 @@ m1 <- nlf(
           trace=1,
           verbose=TRUE,
           eval.only=TRUE,
-          seed=384886L,
+          seed=426094906L,
           lql.frac = 0.025
           )
 se1 <- rep(NA,length(estnames))
@@ -34,10 +34,9 @@ m2 <- nlf(
           maxit=500,
           trace=1,
           verbose=TRUE,
-          seed=384886L,
+          seed=426094906L,
           lql.frac = 0.025
           )
-
 se2 <- m2$se
 
 m3 <- nlf(
@@ -51,7 +50,7 @@ m3 <- nlf(
           trace=1,
           verbose=TRUE,
           eval.only=TRUE,
-          seed=384886L,
+          seed=426094906L,
           lql.frac = 0.025
           )
 se3 <- rep(NA,length(estnames))
@@ -67,17 +66,20 @@ m4 <- nlf(
           maxit=500,
           trace=1,
           verbose=TRUE,
-          seed=384886L,
+          seed=426094906L,
           lql.frac = 0.025
           )
-
 se4 <- m4$se
 
+options(scipen=-3)
 print(
-      cbind(
-            guess=c(theta.guess[estnames],se=se3,value=m3),
-            truth=c(theta.truth[estnames],se=se1,value=m1),
-            fit.from.guess=c(m4$params[estnames],se=se4,value=m4$value),
-            fit.from.truth=c(m2$params[estnames],se=se2,value=m2$value)
-            )
+      signif(
+             cbind(
+                   guess=c(theta.guess[estnames],se=se3,value=m3),
+                   truth=c(theta.truth[estnames],se=se1,value=m1),
+                   fit.from.guess=c(m4$params[estnames],se=se4,value=m4$value),
+                   fit.from.truth=c(m2$params[estnames],se=se2,value=m2$value)
+                   ),
+             4
+             )
       )
