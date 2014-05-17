167c66fdfd38852758309b5bd8b1ef5bcd03e869
Aaron A. King <kingaa@umich.edu>
2014-05-17 21:23:27 +0000

    - add compare.abc and compare.pmcmc methods
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@957 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/R/abc-methods.R
+++ pkg/pomp/R/abc-methods.R
@@ -23,3 +23,56 @@ setMethod(
             }
           }
           )
+
+compare.abc <- function (z) {
+  ## assumes that z is a list of abcs with identical structure
+  if (!is.list(z)) z <- list(z)
+  if (!all(sapply(z,function(x)is(x,'abc'))))
+    stop("compare.abc error: ",
+         sQuote("z"),
+         " must be a pmcmc object or a list of pmcmc objects",call.=FALSE)
+  mar.multi <- c(0,5.1,0,2.1)
+  oma.multi <- c(6,0,5,0)
+  xx <- z[[1]]
+  estnames <- xx@pars
+  parnames <- names(coef(xx))
+  unestnames <- parnames[-match(estnames,parnames)]
+
+  ## plot pmcmc convergence diagnostics
+  other.diagnostics <- c()
+  plotnames <- c(other.diagnostics,estnames)
+  nplots <- length(plotnames)
+  n.per.page <- min(nplots,10)
+  nc <- if (n.per.page<=4) 1 else 2
+  nr <- ceiling(n.per.page/nc)
+  oldpar <- par(mar=mar.multi,oma=oma.multi,mfcol=c(nr,nc))
+  on.exit(par(oldpar)) 
+  low <- 1
+  hi <- 0
+  iteration <- seq(0,xx@Nabc)
+  while (hi<nplots) {
+    hi <- min(low+n.per.page-1,nplots)
+    for (i in seq(from=low,to=hi,by=1)) {
+      n <- i-low+1
+      dat <- sapply(z,function(po,label) conv.rec(po,label),label=plotnames[i])
+      matplot(
+              y=dat, 
+              x=iteration,
+              axes = FALSE,
+              xlab = "",
+              ylab = "",
+              type = "l"
+              )
+      box()
+      y.side <- 2
+      axis(y.side,xpd=NA)
+      mtext(plotnames[i],y.side,line=3)
+      do.xax <- (n%%nr==0||n==n.per.page)
+      if (do.xax) axis(1,xpd=NA)
+      if (do.xax) mtext("ABC iteration",side=1,line=3)
+    }  
+    low <- hi+1
+    mtext("ABC convergence diagnostics",3,line=2,outer=TRUE)
+  }
+  invisible(NULL)
+}
