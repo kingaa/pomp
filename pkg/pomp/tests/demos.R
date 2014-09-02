4948bcfca3fc15b303e2f709de1becbae9252807
Aaron A. King <kingaa@umich.edu>
2014-09-02 15:32:24 +0000

    - run demos during check even under windows
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@1004 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/tests/demos.R
+++ pkg/pomp/tests/demos.R
@@ -1,18 +1,14 @@
-if (Sys.getenv("POMP_FULL_TESTS")=="yes") {
+library(pomp)
 
-  library(pomp)
+pdf.options(useDingbats=FALSE)
+pdf(file="demos.pdf")
 
-  pdf.options(useDingbats=FALSE)
-  pdf(file="demos.pdf")
+set.seed(47575684)
 
-  set.seed(47575684)
+demos <- list.files(path=system.file("demo",package="pomp"),pattern=".\\.R$",full.names=TRUE)
 
-  demos <- list.files(path=system.file("demo",package="pomp"),pattern=".\\.R$",full.names=TRUE)
-
-  for (d in demos) {
-    source(d,local=TRUE,echo=TRUE)
-  }
-
-  dev.off()
-
+for (d in demos) {
+  source(d,local=TRUE,echo=TRUE)
 }
+
+dev.off()
