33854ccadcaf8ad779b3c6bffbb3f75750277356
Aaron A. King <kingaa@umich.edu>
2015-02-22 19:43:06 +0000

    - update proposals
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@1084 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pomp/R/proposals.R
+++ pkg/pomp/R/proposals.R
@@ -1,25 +1,33 @@
-mvn.rw.proposal.fn <- function (rw.sd) {
-  parnm <- colnames(rw.sd)
-  n <- ncol(rw.sd)
+mvn.diag.rw <- function (rw.sd) {
+  if (!is.numeric(rw.sd)) {
+    stop(sQuote("rw.sd")," must be a named numeric vector")
+  }
+  rw.sd <- rw.sd[rw.sd>0]
+  parnm <- names(rw.sd)
+  n <- length(rw.sd)
   if (is.null(parnm))
     stop(sQuote("rw.sd")," must have names")
-  if (is.matrix(rw.sd)) {
-    if (nrow(rw.sd)!=ncol(rw.sd))
-      stop(sQuote("rw.sd")," must be a square matrix")
-    ch <- try (chol(rw.sd,pivot=TRUE))
-    if (inherits(ch,"try-error"))
-      stop("error in Choleski factorization of ",sQuote("rw.sd"))
-    oo <- order(attr(ch,"pivot"))
-    Q <- ch[,oo]
-    fn <- function (theta) {
-      theta[parnm] <- theta[parnm]+Q.rnorm(n=n,mean=0,sd=1)
-      theta
-    }
-  } else {
-    fn <- function (theta) {
-      theta[parnm] <- rnorm(n=n,mean=theta[parnm],sd=rw.sd)
-      theta
-    }
+  function (theta) {
+    theta[parnm] <- rnorm(n=n,mean=theta[parnm],sd=rw.sd)
+    theta
   }
-  fn
+}
+
+mvn.rw <- function (rw.var) {
+  rw.var <- as.matrix(rw.var)
+  parnm <- colnames(rw.var)
+  n <- ncol(rw.var)
+  if (is.null(parnm))
+    stop(sQuote("rw.var")," must have row- and column-names")
+  if (nrow(rw.var)!=ncol(rw.var))
+    stop(sQuote("rw.var")," must be a square matrix")
+  ch <- try (chol(rw.var,pivot=TRUE))
+  if (inherits(ch,"try-error"))
+    stop("error in Choleski factorization of ",sQuote("rw.var"))
+  oo <- order(attr(ch,"pivot"))
+  Q <- ch[,oo]
+  function (theta) {
+    theta[parnm] <- theta[parnm]+Q%*%rnorm(n=n,mean=0,sd=1)
+    theta
+  }
 }
