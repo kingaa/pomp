e300a47502c6c28f1f480569d61fafcd89c9dc9f
Aaron A. King <kingaa@umich.edu>
2015-03-01 03:41:09 +0000

    - remove advanced topics vignette - add getting_started vignette - update Makefile
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/pomp@1120 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ www/vignettes/getting_started.R
@@ -0,0 +1,215 @@
+## ----opts,include=FALSE--------------------------------------------------
+library(knitr)
+prefix <- "getting-started"
+opts_chunk$set(
+  progress=TRUE,
+  prompt=FALSE,tidy=FALSE,highlight=TRUE,
+  strip.white=TRUE,
+  warning=FALSE,
+  message=FALSE,
+  error=FALSE,
+  echo=TRUE,
+  cache=TRUE,
+  results='markup',
+  fig.show='asis',
+  size='small',
+  fig.lp="fig:",
+  fig.path=paste0("figure/",prefix,"-"),
+  cache.path=paste0("cache/",prefix,"-"),
+  fig.pos="h!",
+  fig.align='center',
+  fig.height=4,fig.width=6.83,
+  out.width="\\linewidth",
+  dpi=300,
+  dev='png',
+  dev.args=list(bg='transparent')
+  )
+
+## ----prelims,echo=F,cache=F----------------------------------------------
+set.seed(594709947L)
+require(ggplot2)
+require(plyr)
+require(reshape2)
+require(magrittr)
+require(pomp)
+theme_set(theme_bw())
+stopifnot(packageVersion("pomp")>="0.62-1")
+options(
+  keep.source=TRUE,
+  stringsAsFactors=FALSE,
+  encoding="UTF-8"
+  )
+
+## ----eval=FALSE----------------------------------------------------------
+## install.packages("pomp",repos="http://R-Forge.R-Project.org")
+
+## ----eval=FALSE----------------------------------------------------------
+## cat("#include <R.h>
+##     void hello (void) {
+##     Rprintf(\"hello world!\\n\");
+##     }",file="hello.c")
+## system("R CMD SHLIB hello.c")
+## dyn.load(paste0("hello",.Platform$dynlib.ext))
+## .C("hello",PACKAGE="hello")
+
+## ----load-parus-data-----------------------------------------------------
+parus.dat <- read.csv(text="
+                      year,pop
+                      1960,148
+                      1961,258
+                      1962,185
+                      1963,170
+                      1964,267
+                      1965,239
+                      1966,196
+                      1967,132
+                      1968,167
+                      1969,186
+                      1970,128
+                      1971,227
+                      1972,174
+                      1973,177
+                      1974,137
+                      1975,172
+                      1976,119
+                      1977,226
+                      1978,166
+                      1979,161
+                      1980,199
+                      1981,306
+                      1982,206
+                      1983,350
+                      1984,214
+                      1985,175
+                      1986,211"
+                      )
+
+## ----parus-plot----------------------------------------------------------
+ggplot(data=parus.dat,mapping=aes(x=year,y=pop))+
+  geom_line()+geom_point()+
+  expand_limits(y=0)+
+  theme_classic()
+
+## ----logistic-step-fun---------------------------------------------------
+step.fun <- Csnippet("
+  double dW = rnorm(0,sqrt(dt));
+  N += r*N*(1-N/K)*dt+sigma*N*dW;
+  ")
+
+## ----logistic-pomp1,cache=FALSE------------------------------------------
+parus <- pomp(data=parus.dat,time="year",t0=1959,
+              rprocess=euler.sim(step.fun=step.fun,delta.t=1/365),
+              statenames="N",paramnames=c("r","K","sigma"))
+
+## ----logistic-simul1-----------------------------------------------------
+simStates <- simulate(parus,nsim=10,params=c(r=0.2,K=200,sigma=0.5,N.0=200),states=TRUE)
+
+## ----logistic-plot1,echo=FALSE-------------------------------------------
+melt(simStates) %>% 
+  dcast(rep+time~variable) %>%
+  ggplot(mapping=aes(x=time,y=N,group=rep,color=factor(rep)))+
+  geom_line()+guides(color=FALSE)+
+  theme_classic()
+
+## ----logistic-rmeasure---------------------------------------------------
+rmeas <- Csnippet("
+  pop = rpois(phi*N);
+  ")
+
+## ----logistic-pomp2,cache=FALSE------------------------------------------
+parus <- pomp(parus,rmeasure=rmeas,paramnames="phi",statenames="N")
+
+## ----logistic-simul2-----------------------------------------------------
+sim <- simulate(parus,params=c(r=0.2,K=200,phi=0.5,sigma=0.5,N.0=200),
+                nsim=10,obs=TRUE,states=TRUE)
+
+## ----logistic-plot2,echo=FALSE-------------------------------------------
+sim %>% melt() %>% 
+  ggplot(mapping=aes(x=time,y=value,group=rep,color=factor(rep)))+
+  geom_line()+
+  guides(color=FALSE)+scale_y_sqrt()+
+  facet_grid(variable~.,scales="free_y")
+
+sim %>% melt() %>% dcast(rep+time~variable,value.var='value') %>%
+  ggplot(mapping=aes(x=N,y=pop,color=factor(rep)))+
+  geom_point()+scale_x_sqrt()+scale_y_sqrt()+
+  coord_equal()+
+  guides(color=FALSE)
+
+## ----logistic-dmeasure---------------------------------------------------
+dmeas <- Csnippet("
+  lik = dpois(pop,phi*N,give_log);
+  ")
+
+## ----logistic-pomp3,cache=FALSE------------------------------------------
+parus <- pomp(parus,dmeasure=dmeas,paramnames="phi",statenames="N")
+
+## ----logistic-pfilter----------------------------------------------------
+pf <- pfilter(parus,Np=1000,params=c(r=0.2,K=200,phi=0.5,sigma=0.5,N.0=200))
+logLik(pf)
+
+## ----logistic-skeleton,cache=FALSE---------------------------------------
+skel <- Csnippet("
+  DN = r*N*(1-N/K);
+  ")
+
+parus <- pomp(parus,skeleton=skel,skeleton.type="vectorfield",statenames="N",paramnames=c("r","K"))
+
+## ----logistic-traj1------------------------------------------------------
+pars <- parmat(c(r=1,K=200,phi=0.5,sigma=0.5,N.0=20),5)
+pars["N.0",] <- seq(20,300,length=5)
+traj <- trajectory(parus,params=pars,times=seq(1959,1970,by=0.01))
+
+## ----logistic-plot3,echo=FALSE-------------------------------------------
+trajectory(parus,params=pars,times=seq(1959,1970,by=0.01),as.data.frame=TRUE) %>%
+  ggplot(mapping=aes(x=time,y=N,group=traj,color=traj))+
+  guides(color=FALSE)+
+  geom_line()
+
+## ----bh-stepfun----------------------------------------------------------
+bh.step <- Csnippet("
+  double eps = rlnorm(-sigma*sigma/2,sigma);
+  N = a*N/(1+b*N)*eps;
+  ")
+
+## ----bh-skeleton---------------------------------------------------------
+bh.skel <- Csnippet("
+  DN = a*N/(1+b*N);
+  ")
+
+## ----bh-pomp1,cache=FALSE------------------------------------------------
+parus.bh <- pomp(parus,rprocess=discrete.time.sim(bh.step,delta.t=1),skeleton=bh.skel,skeleton.type="map",skelmap.delta.t=1,statenames="N",paramnames=c("a","b","sigma"))
+
+## ----bh-test-------------------------------------------------------------
+coef(parus.bh) <- c(a=1.1,b=5e-4,sigma=0.5,N.0=30,phi=1)
+sim <- simulate(parus.bh)
+traj <- trajectory(parus.bh)
+pf <- pfilter(parus.bh,Np=1000)
+
+## ----logistic-partrans,cache=FALSE---------------------------------------
+partrans <- Csnippet("
+  Tr = exp(r);
+  TK = exp(K);
+  ")
+
+parinvtrans <- Csnippet("
+  Tr = log(r);
+  TK = log(K);
+  ")
+
+parus <- pomp(parus,parameter.transform=partrans,parameter.inv.transform=parinvtrans,
+              paramnames=c("r","K"))
+
+## ----logistic-partrans-test,include=FALSE,cache=FALSE--------------------
+p <- c(r=1,K=200,phi=1,N.0=200,sigma=0.5)
+coef(parus,transform=TRUE) <- partrans(parus,p,dir="inv")
+stopifnot(all.equal(p,coef(parus)))
+
+## ----tempfile-view,eval=FALSE--------------------------------------------
+## op <- options(verbose=TRUE)
+## 
+## parus <- pomp(data=parus.dat,times="year",t0=1959,rprocess=euler.sim(step.fun,delta.t=1/365),
+##      dmeasure=dmeas,rmeasure=rmeas,statenames="N",paramnames=c("r","phi","K","N.0","sigma"))
+## 
+## options(op)
+
