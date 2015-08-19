4dc3bdca1cfcb00f9283c2f5dd49141f48849a03
Aaron A. King <kingaa@umich.edu>
2015-08-19 08:16:58 -0400

    - update test

--- tests/getting_started.R
+++ tests/getting_started.R
@@ -1,4 +1,4 @@
-## ----prelims,echo=F,cache=F----------------------------------------------
+## ----prelims,echo=FALSE,cache=FALSE--------------------------------------
 require(pomp)
 options(
   keep.source=TRUE,
@@ -6,24 +6,18 @@ options(
   encoding="UTF-8",
   scipen=5
   )
+set.seed(594709947L)
 
-set.seed(588665L)
-## ----eval=FALSE----------------------------------------------------------
-## require(devtools)
-## install_github("kingaa/pomp")
-
-## ----eval=FALSE----------------------------------------------------------
-## cat("#include <R.h>
-##     void hello (void) {
-##     Rprintf(\"hello world!\\n\");
-##     }",file="hello.c")
-## system("R CMD SHLIB hello.c")
-## dyn.load(paste0("hello",.Platform$dynlib.ext))
-## .C("hello",PACKAGE="hello")
+## ----prelims2,echo=FALSE,cache=FALSE-------------------------------------
+require(ggplot2)
+require(plyr)
+require(reshape2)
+require(magrittr)
+theme_set(theme_bw())
 
 ## ----load-parus-data-----------------------------------------------------
 parus.dat <- read.csv(text="
-                      year,pop
+                      year,P
                       1960,148
                       1961,258
                       1962,185
@@ -53,13 +47,19 @@ parus.dat <- read.csv(text="
                       1986,211"
                       )
 
+## ----parus-plot----------------------------------------------------------
+ggplot(data=parus.dat,mapping=aes(x=year,y=P))+
+  geom_line()+geom_point()+
+  expand_limits(y=0)+
+  theme_classic()
+
 ## ----logistic-step-fun---------------------------------------------------
 step.fun <- Csnippet("
   double dW = rnorm(0,sqrt(dt));
   N += r*N*(1-N/K)*dt+sigma*N*dW;
 ")
 
-## ----logistic-pomp1,cache=FALSE------------------------------------------
+## ----logistic-pomp1------------------------------------------------------
 parus <- pomp(data=parus.dat,time="year",t0=1959,
               rprocess=euler.sim(step.fun=step.fun,delta.t=1/365),
               statenames="N",paramnames=c("r","K","sigma"))
@@ -67,31 +67,51 @@ parus <- pomp(data=parus.dat,time="year",t0=1959,
 ## ----logistic-simul1-----------------------------------------------------
 simStates <- simulate(parus,nsim=10,params=c(r=0.2,K=200,sigma=0.5,N.0=200),states=TRUE)
 
+## ----logistic-plot1,echo=FALSE-------------------------------------------
+melt(simStates) %>% 
+  dcast(rep+time~variable) %>%
+  ggplot(mapping=aes(x=time,y=N,group=rep,color=factor(rep)))+
+  geom_line()+guides(color=FALSE)+
+  theme_classic()
+
 ## ----logistic-rmeasure---------------------------------------------------
 rmeas <- Csnippet("
-  pop = rpois(phi*N);
+  P = rpois(N);
 ")
 
-## ----logistic-pomp2,cache=FALSE------------------------------------------
-parus <- pomp(parus,rmeasure=rmeas,paramnames="phi",statenames="N")
+## ----logistic-pomp2------------------------------------------------------
+parus <- pomp(parus,rmeasure=rmeas,statenames="N")
 
 ## ----logistic-simul2-----------------------------------------------------
-sim <- simulate(parus,params=c(r=0.2,K=200,phi=0.5,sigma=0.5,N.0=200),
+sim <- simulate(parus,params=c(r=0.2,K=200,sigma=0.5,N.0=200),
                 nsim=10,obs=TRUE,states=TRUE)
+
+## ----logistic-plot2,echo=FALSE-------------------------------------------
+sim %>% melt() %>% 
+  ggplot(mapping=aes(x=time,y=value,group=rep,color=factor(rep)))+
+  geom_line()+
+  guides(color=FALSE)+scale_y_sqrt()+
+  facet_grid(variable~.,scales="free_y")
+
+sim %>% melt() %>% dcast(rep+time~variable,value.var='value') %>%
+  ggplot(mapping=aes(x=N,y=P,color=factor(rep)))+
+  geom_point()+scale_x_sqrt()+scale_y_sqrt()+
+  coord_equal()+
+  guides(color=FALSE)
 
 ## ----logistic-dmeasure---------------------------------------------------
 dmeas <- Csnippet("
-  lik = dpois(pop,phi*N,give_log);
+  lik = dpois(P,N,give_log);
 ")
 
-## ----logistic-pomp3,cache=FALSE------------------------------------------
-parus <- pomp(parus,dmeasure=dmeas,paramnames="phi",statenames="N")
+## ----logistic-pomp3------------------------------------------------------
+parus <- pomp(parus,dmeasure=dmeas,statenames="N")
 
 ## ----logistic-pfilter----------------------------------------------------
-pf <- pfilter(parus,Np=1000,params=c(r=0.2,K=200,phi=0.5,sigma=0.5,N.0=200))
+pf <- pfilter(parus,Np=1000,params=c(r=0.2,K=200,sigma=0.5,N.0=200))
 logLik(pf)
 
-## ----logistic-skeleton,cache=FALSE---------------------------------------
+## ----logistic-skeleton---------------------------------------------------
 skel <- Csnippet("
   DN = r*N*(1-N/K);
 ")
@@ -99,9 +119,15 @@ skel <- Csnippet("
 parus <- pomp(parus,skeleton=skel,skeleton.type="vectorfield",statenames="N",paramnames=c("r","K"))
 
 ## ----logistic-traj1------------------------------------------------------
-pars <- parmat(c(r=1,K=200,phi=0.5,sigma=0.5,N.0=20),5)
+pars <- parmat(c(r=1,K=200,sigma=0.5,N.0=20),5)
 pars["N.0",] <- seq(20,300,length=5)
 traj <- trajectory(parus,params=pars,times=seq(1959,1970,by=0.01))
+
+## ----logistic-plot3,echo=FALSE-------------------------------------------
+trajectory(parus,params=pars,times=seq(1959,1970,by=0.01),as.data.frame=TRUE) %>%
+  ggplot(mapping=aes(x=time,y=N,group=traj,color=traj))+
+  guides(color=FALSE)+
+  geom_line()
 
 ## ----bh-stepfun----------------------------------------------------------
 bh.step <- Csnippet("
@@ -114,49 +140,54 @@ bh.skel <- Csnippet("
   DN = a*N/(1+b*N);
 ")
 
-## ----bh-pomp1,cache=FALSE------------------------------------------------
+## ----bh-pomp1------------------------------------------------------------
 parus.bh <- pomp(parus,rprocess=discrete.time.sim(bh.step,delta.t=1),skeleton=bh.skel,skeleton.type="map",skelmap.delta.t=1,statenames="N",paramnames=c("a","b","sigma"))
 
 ## ----bh-test-------------------------------------------------------------
-coef(parus.bh) <- c(a=1.1,b=5e-4,sigma=0.5,N.0=30,phi=1)
+coef(parus.bh) <- c(a=1.1,b=5e-4,sigma=0.5,N.0=30)
 sim <- simulate(parus.bh)
 traj <- trajectory(parus.bh)
 pf <- pfilter(parus.bh,Np=1000)
 
-## ----logistic-partrans,cache=FALSE---------------------------------------
+## ----logistic-partrans---------------------------------------------------
 logtrans <- Csnippet("
   Tr = log(r);
   TK = log(K);
+  Tsigma = log(sigma);
 ")
 
 exptrans <- Csnippet("
   Tr = exp(r);
   TK = exp(K);
+  Tsigma = exp(sigma);
 ")
 
 parus <- pomp(parus,toEstimationScale=logtrans,
               fromEstimationScale=exptrans,
-              paramnames=c("r","K"))
+              paramnames=c("r","K","sigma"))
 
-## ----logistic-partrans-test,include=FALSE,cache=FALSE--------------------
-p <- c(r=1,K=200,phi=1,N.0=200,sigma=0.5)
+## ----logistic-partrans-test,include=FALSE--------------------------------
+p <- c(r=1,K=200,N.0=200,sigma=0.5)
 coef(parus,transform=TRUE) <- partrans(parus,p,dir="inv")
 stopifnot(all.equal(p,coef(parus)))
 
-## ----parus-traj-match,cache=FALSE----------------------------------------
-tm <- traj.match(parus,start=c(r=1,K=200,phi=1,N.0=200,sigma=0.5),
-                 est=c("r","K","phi"),transform=TRUE)
+## ----parus-traj-match----------------------------------------------------
+tm <- traj.match(parus,start=c(r=1,K=200,N.0=200,sigma=0.5),
+                 est=c("r","K"),transform=TRUE)
 signif(coef(tm),3)
 logLik(tm)
 
-## ----parus-tm-sim1,cache=FALSE-------------------------------------------
+## ----parus-tm-sim1-------------------------------------------------------
 coef(tm,"sigma") <- 0
-sim1 <- simulate(tm,nsim=10,as.data.frame=TRUE,include.data=TRUE)
+simulate(tm,nsim=10,as.data.frame=TRUE,include.data=TRUE) %>%
+  ggplot(aes(x=time,y=P,group=sim,alpha=(sim=="data")))+
+  scale_alpha_manual(name="",values=c(`TRUE`=1,`FALSE`=0.2),
+                     labels=c(`FALSE`="simulation",`TRUE`="data"))+
+  geom_line()
 
-## ----parus-mif,cache=TRUE------------------------------------------------
-mf <- mif2(parus,Nmif=50,Np=1000,cooling.fraction=0.8,
-           rw.sd=rw.sd(r=0.02,K=0.02,phi=0.02,sigma=0.02),transform=TRUE)
-mf <- mif2(mf)
-mle <- coef(mf); mle
-logmeanexp(replicate(5,logLik(pfilter(mf))),se=TRUE)
-sim2 <- simulate(mf,nsim=10,as.data.frame=TRUE,include.data=TRUE)
+## ----parus-dprior--------------------------------------------------------
+parus %<>%
+  pomp(dprior=Csnippet("
+    lik = dunif(r,0,5,1)+dunif(K,100,600,1)+dunif(sigma,0,2,1);
+    lik = (give_log) ? lik : exp(lik);
+  "),paramnames=c("r","K","sigma"))
