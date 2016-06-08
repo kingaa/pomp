deed2464874784e307da409b6158ca8f2417179b
Aaron A. King <kingaa@umich.edu>
2016-06-08 14:02:13 -0400

    save figures from test 'demos.R'

--- tests/demos.R
+++ tests/demos.R
@@ -1,11 +1,12 @@
 library(pomp)
 
-pdf.options(useDingbats=FALSE)
-pdf(file="demos.pdf")
+png(filename="demos-%02d.png",res=100)
 
-set.seed(47575684)
+set.seed(47575684L)
 
-demos <- list.files(path=system.file("demo",package="pomp"),pattern=".\\.R$",full.names=TRUE)
+demos <- list.files(path=system.file("demo",package="pomp"),
+                    pattern=".\\.R$",
+                    full.names=TRUE)
 
 for (d in demos) {
   source(d,local=TRUE,echo=TRUE)
