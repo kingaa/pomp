b67625b5344df9ecaf81c1c90ac9a037f74ecb91
Aaron A. King <kingaa@umich.edu>
2016-07-19 08:50:22 -0400

    revert 0f5ede7: replace automatic verbose option when compiling C snippets under windows

--- R/builder.R
+++ R/builder.R
@@ -213,7 +213,7 @@ pompCompile <- function (fname, direc, src, verbose) {
         command=R.home("bin/R"),
         args=c("CMD","SHLIB","-o",solib,modelfile),
         env=cflags,
-        stdout=if (verbose) "" else NULL
+        stdout=if (verbose | .Platform$OS.type=="windows") "" else NULL
     )
     if (rv!=0)
         stop("cannot compile shared-object library ",sQuote(solib),call.=FALSE)
