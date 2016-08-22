2bc3166a208d826ed9f31a0cc5d2a43474017999
Aaron A. King <kingaa@umich.edu>
2016-08-22 08:01:10 -0400

    make 'sobol' codes more robust

--- src/dsobol.c
+++ src/dsobol.c
@@ -8,22 +8,17 @@ void F77_NAME(gosobl)(double *);
 
 SEXP sobol_sequence (SEXP dim)
 {
-  int nprotect = 0;
   SEXP data;
   int d = INTEGER(dim)[0];
   int n = INTEGER(dim)[1];
-  PROTECT(data = allocMatrix(REALSXP,d,n)); nprotect++;
-  dsobol(REAL(data),d,n);
-  UNPROTECT(nprotect);
-  return(data);
-}
-
-static void dsobol (double *data, int dim, int n)
-{
+  double *dp;
   int flag[2], taus = 0;
   int k;
-  F77_CALL(insobl)(flag, &dim, &n, &taus);
+  F77_CALL(insobl)(flag, &d, &n, &taus); // error check and setup 
   if (!flag[0]) errorcall(R_NilValue,"dimension is too high");
   if (!flag[1]) errorcall(R_NilValue,"too many points requested");
-  for (k = 0; k < n; k++) F77_CALL(gosobl)(data + k*dim);
+  PROTECT(data = allocMatrix(REALSXP,d,n)); dp = REAL(data);
+  for (k = 0; k < n; k++) F77_CALL(gosobl)(dp + k*d); // compute
+  UNPROTECT(1);
+  return(data);
 }
