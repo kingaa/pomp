2cdfeccb6fb270ae66236e2c703d2bfeb56ca4da
Aaron A. King <kingaa@umich.edu>
2017-08-09 07:52:54 -0400

    load pomp at appropriate place

--- vignettes/getting_started.R
+++ vignettes/getting_started.R
@@ -7,7 +7,6 @@ registerDoMPI(cl)
 registerDoRNG(348885445L)
 
 ## ----prelims,echo=FALSE,cache=FALSE--------------------------------------
-library(pomp)
 stopifnot(packageVersion("pomp")>="1.11")
 options(
   keep.source=TRUE,
@@ -57,6 +56,7 @@ ggplot(data=parus.dat,mapping=aes(x=year,y=P))+
   theme_bw()
 
 ## ----logistic-step-fun---------------------------------------------------
+library(pomp)
 step.fun <- Csnippet("
   double dW = rnorm(0,sqrt(dt));
   N += r*N*(1-N/K)*dt+sigma*N*dW;
