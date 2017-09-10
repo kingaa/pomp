fbfa94e25b5a6af5d3ad01bcc0666e9e296e78c8
Aaron A. King <kingaa@umich.edu>
2017-09-10 13:24:24 -0400

    partial reversion

--- R/pomp_methods.R
+++ R/pomp_methods.R
@@ -24,8 +24,6 @@ setAs(
 
 as.data.frame.pomp <- function (x, row.names, optional, ...) as(x,"data.frame")
 
-setIs("pomp","data.frame")
-
 ## parameter transformations
 partrans.internal <- function (object, params,
                                dir = c("fromEstimationScale",
