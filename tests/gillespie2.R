fedcf7bdbb2f23308a0ff477d90b9723f3f09297
Eamon O'Dea <odea35@gmail.com>
2017-09-24 13:42:58 -0400

    compare hl.sim and sim

--- tests/gillespie2.R
+++ tests/gillespie2.R
@@ -70,6 +70,143 @@ try(mwe %>% pomp(rprocess=gillespie.hl.sim(list(c("rate = mu * N;"),
                  zeronames="ct", paramnames=c("mu"),
                  statenames=c("N","ct")) %>% simulate() %>% invisible())
 
+library(ggplot2)
+library(reshape2)
+
+png(filename="gillespie2-%02d.png",res=100)
+
+c(gamma=12,mu=1/70,iota=0.1,
+  beta1=330,beta2=410,beta3=490,
+  beta.sd=0,
+  rho=0.1,
+  S_0=0.07,I_0=1e-4,R_0=0.93,
+  pop=1000000
+) -> params
+
+rate.fun <- function(j, x, t, params, covars, ...) {
+  switch(
+    j,
+    params["mu"]*x["N"],            # birth
+    params["mu"]*x["S"],            # susceptible death
+    {                               # infection
+      beta <- sum(covars*params[c("beta1","beta2","beta3")])
+      (beta*x["I"]+params["iota"])*x["S"]/x["N"]
+    },
+    params["mu"]*x["I"],            # infected death
+    params["gamma"]*x["I"],         # recovery
+    params["mu"]*x["R"],           # recovered death
+    stop("unrecognized event ",j)
+  )
+}
+
+rate.fun.snip <- Csnippet("
+  double beta;
+  switch (j) {
+  case 1: 			// birth
+    rate = mu * N;
+    break;
+  case 2:			// susceptible death
+    rate = mu * S;
+    break;
+  case 3:			// infection
+    beta = seas_1 * beta1 + seas_2 * beta2 + seas_3 * beta3;
+    rate = (beta * I + iota) * S / N;
+    break;
+  case 4:			// infected death
+    rate = mu * I;
+    break;
+  case 5:			// recovery
+    rate = gamma * I;
+    break;
+  case 6:			// recovered death
+    rate = mu * R;
+    break;
+  default:
+    error(\"unrecognized event %d\",j);
+    break;
+  }")
+
+cbind(
+  birth=c(1,0,0,1,0),
+  sdeath=c(-1,0,0,-1,0),
+  infection=c(-1,1,0,0,0),
+  ideath=c(0,-1,0,-1,0),
+  recovery=c(0,-1,1,0,1),
+  rdeath=c(0,0,-1,-1,0)
+) -> Vmatrix
+
+data.frame(
+  time=seq(from=0,to=2,by=1/52),
+  reports=NA
+) %>%
+  pomp(
+    times="time",
+    t0=0,
+    rprocess=gillespie.sim(rate.fun=rate.fun,v=Vmatrix,hmax=1/52/10),
+    zeronames=c("cases"),
+    covar=data.frame(
+      t=seq(0,2,by=1/52/10),
+      seas=periodic.bspline.basis(
+        seq(0,2,by=1/52/10),
+        degree=3,period=1,nbasis=3)),
+    tcovar="t",
+    measurement.model=reports~binom(size=cases,prob=rho),
+    initializer=function(params, t0, ...){
+      comp.names <- c("S","I","R")
+      icnames <- paste(comp.names,"0",sep="_")
+      snames <- c("S","I","R","N","cases")
+      fracs <- params[icnames]
+      x0 <- numeric(length(snames))
+      names(x0) <- snames
+      x0["N"] <- params["pop"]
+      x0[comp.names] <- round(params['pop']*fracs/sum(fracs))
+      x0
+    }
+  ) %>%
+  simulate(params=params,seed=806867104L) -> gsir
+
+gsir %>%
+    pomp(rprocess=gillespie.sim(rate.fun=rate.fun.snip,
+                                v=Vmatrix,hmax=1/52/10),
+         paramnames = names(params),
+         statenames = c("S","I","R", "N", "cases"),
+         ) %>%
+    simulate(seed=806867104L) -> gsir1
+
+pompExample(gillespie.sir)
+gsir2 <- simulate(gillespie.sir,params=coef(gsir),
+                  times=time(gsir),t0=timezero(gsir),seed=806867104L)
+
+hl.args <- list(list("rate = mu * N;",    c(S= 1,I= 0,R= 0,N= 1,cases= 0)),
+                list("rate = mu * S;",    c(S=-1,I= 0,R= 0,N=-1,cases= 0)),
+                list("rate = mu * I;",    c(S= 0,I=-1,R= 0,N=-1,cases= 0)),
+                list("rate = mu * R;",    c(S= 0,I= 0,R=-1,N=-1,cases= 0)),
+                list("rate = gamma * I;", c(S= 0,I=-1,R= 1,N= 0,cases= 1)),
+                list(paste("double beta;",
+                           "beta = seas_1 * beta1 + seas_2 * beta2 + seas_3 * beta3;",
+                           "rate = (beta * I + iota) * S / N;", sep = "\n"),
+                     c(S=-1,I= 1,R= 0,N= 0,cases= 0)))
+
+gsir %>%
+    pomp(rprocess=do.call(gillespie.hl.sim, c(hl.args, list(hmax=1/52/10))),
+         paramnames = names(params),
+         statenames = c("S","I","R", "N", "cases"),
+         ) %>%
+    simulate(seed=806867104L) -> gsirhl
+
+list(gill.sim.R=as.data.frame(gsir),
+     gill.sim.Csnippet=as.data.frame(gsir1),
+     gill.hl.sim=as.data.frame(gsirhl)) %>%
+  melt(id="time") %>%
+  subset(variable=="reports") %>%
+  ggplot(aes(x=time,y=value,color=L1))+
+  labs(color="",y="reports",title="various implementations of same SIR model")+
+  geom_line()+
+  theme_bw()+theme(legend.position=c(0.2,0.8))
+
+all.equal(as.data.frame(gsir), as.data.frame(gsir1))
+
 ## vnames in different order than statevars
 ## order of names for each elementary event different
 ## Cnsippets in hl.sim, .sim, and R function in .sim all are consistent
+## use .pre and .post
