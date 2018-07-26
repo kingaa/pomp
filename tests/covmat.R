25f28c8d0c66ccf3b76cdca5712ab7e8b650a2a4
Aaron A. King <kingaa@umich.edu>
2018-07-26 15:57:08 -0400

    covmat tests

---
+++ tests/covmat.R
@@ -0,0 +1,51 @@
+options(digits=3)
+
+library(pomp)
+library(magrittr)
+library(tidyr)
+library(dplyr)
+
+pompExample(gompertz)
+
+set.seed(1546383977L)
+
+plist <- list(
+  y1.mean=probe.mean(var="Y"),
+  probe.acf(var="Y",lags=c(0,4,8))
+)
+
+gompertz %>%
+  probe(probes=plist,nsim=100) %>%
+  values() %>%
+  filter(.id=="sim") %>%
+  select(-.id) %>%
+  gather(variable,value) %>%
+  group_by(variable) %>%
+  summarize(value=(sd(value))) %>%
+  ungroup() %>%
+  spread(variable,value) %>%
+  unlist() -> scale.dat
+
+abc(
+  gompertz,
+  dprior=Csnippet("
+    lik = dunif(K,0,2,1)+dunif(r,0,1,1)+
+      dunif(tau,0,1,1)+dunif(sigma,0,1,1);
+    lik = (give_log) ? lik : exp(lik);"),
+  paramnames=c("K","sigma","tau","r"),
+  Nabc=100,probes=plist,
+  scale=scale.dat,epsilon=10,
+  proposal=mvn.diag.rw(c(K=0.01,r=0.01,sigma=0.01,tau=0.01))
+) -> a1
+
+replicate(3,
+  abc(
+    a1,
+    Nabc=500,probes=plist,
+    scale=scale.dat,epsilon=10,
+    proposal=mvn.diag.rw(c(K=0.01,r=0.01,sigma=0.01,tau=0.01))
+  )) -> a1
+do.call(c,a1) -> a1
+
+covmat(a1[[1]])
+covmat(a1)
