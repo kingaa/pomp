44ec8bea6c08313a98ed03f65d281f317c660db4
Aaron A. King <kingaa@umich.edu>
2018-07-26 11:11:02 -0400

    dmeasure tests

--- R/dmeasure_pomp.R
+++ R/dmeasure_pomp.R
@@ -1,18 +1,21 @@
 ## evaluate the measurement model density function
 
-dmeasure.internal <- function (object, y, x, times, params, log = FALSE, .getnativesymbolinfo = TRUE, ...) {
-    storage.mode(y) <- "double"
-    storage.mode(x) <- "double"
-    storage.mode(params) <- "double"
-    pompLoad(object)
-    rv <- .Call(do_dmeasure,object,y,x,times,params,log,.getnativesymbolinfo)
-    pompUnload(object)
-    rv
+dmeasure.internal <- function (object, y, x, times, params, log = FALSE,
+                               .getnativesymbolinfo = TRUE, ...) {
+  storage.mode(y) <- "double"
+  storage.mode(x) <- "double"
+  storage.mode(params) <- "double"
+  pompLoad(object)
+  rv <- .Call(do_dmeasure,object,y,x,times,params,log,.getnativesymbolinfo)
+  pompUnload(object)
+  rv
 }
 
 setMethod(
-    "dmeasure",
-    signature=signature(object="pomp"),
-    definition=function (object, y, x, times, params, log = FALSE, ...)
-        dmeasure.internal(object=object,y=y,x=x,times=times,params=params,log=log,...)
+  "dmeasure",
+  signature=signature(object="pomp"),
+  definition=function (object, y, x, times, params, log = FALSE, ...) {
+    dmeasure.internal(object=object,y=y,x=x,times=times,
+                      params=params,log=log,...)
+  }
 )
---
+++ tests/dmeasure.R
@@ -0,0 +1,68 @@
+library(pomp)
+
+pompExample(ou2)
+
+po <- window(ou2,end=10)
+
+set.seed(3434388L)
+dat <- simulate(po,obs=T,states=T,nsim=5)
+x <- dat$states
+y <- dat$obs[,1,]
+t <- time(po)
+theta <- coef(po)
+
+dmeasure(po,x=x,y=y,times=t,params=theta) -> L
+dmeasure(po,x=x,y=y,times=t,params=theta,log=T) -> ll
+stopifnot(all.equal(ll[,1:3],log(L[,1:3])))
+stopifnot(identical(dim(ll),c(5L,10L)))
+
+try(dmeasure(x=x,y=y,times=t,params=theta))
+try(dmeasure(x,y=y,times=t,params=theta))
+try(dmeasure(po,x=x,y=y,times=t))
+try(dmeasure(po,x=x,y=y,params=theta))
+try(dmeasure(po,x=x,times=t,params=theta))
+try(dmeasure(po,y=y,times=t,params=theta))
+try(dmeasure(po,x=as.numeric(x),y=y,times=t,params=theta))
+try(dmeasure(po,x=x,y=as.numeric(y),times=t,params=theta))
+try(dmeasure(po,x=x,y=y,times=NULL,params=theta))
+try(dmeasure(po,x=x[,,1],y=y[,1,drop=FALSE],times=t[1],params=theta))
+invisible(dmeasure(po,x=x[,,1,drop=FALSE],y=y[,1],times=t[1],params=theta))
+stopifnot(all.equal(dmeasure(po,x=x[,1,,drop=FALSE],y=y,times=t,params=theta),
+                    dmeasure(po,x=x[,1,],y=y,times=t,params=theta)))
+try(dmeasure(po,x=x,y=y[1,,drop=FALSE],times=t,params=theta))
+try(dmeasure(po,x=x[1,,,drop=FALSE],y=y,times=t,params=theta))
+k <- which(names(theta)=="tau")
+try(dmeasure(po,x=x,y=y,times=t,params=theta[-k]))
+
+xx <- x
+xx[] <- as.integer(round(x))
+stopifnot(all.equal(dmeasure(po,x=xx,y=y,times=t,params=theta,log=TRUE),
+                    dmeasure(po,x=round(x),y=y,times=t,params=theta,log=TRUE)))
+
+stopifnot({
+  dmeasure(pomp(po,dmeasure=function(y,x,t,params,log,...)3),
+           x=x,y=y,times=t,params=theta,log=TRUE) -> z
+  dimnames(z)
+  all(z[]==3)
+})
+
+try(dmeasure(pomp(po,dmeasure=function(y,x,t,params,log,...)c(3,2,1)),
+             x=x,y=y,times=t,params=theta,log=TRUE))
+
+pp <- parmat(theta,10)
+dmeasure(po,params=pp,x=x,y=y,t=t,log=TRUE) -> ll
+stopifnot(all.equal(ll[1:5,],ll[6:10,]))
+try(dmeasure(po,params=pp[,1:7],x=x,y=y,t=t,log=TRUE))
+
+pompExample(dacca)
+set.seed(3434388L)
+po <- window(dacca,end=1892)
+dat <- simulate(po,obs=T,states=T,nsim=5)
+x <- dat$states
+y <- dat$obs[,1,,drop=FALSE]
+t <- time(po)
+theta <- coef(po)
+
+dmeasure(po,x=x,y=y,times=t,params=theta) -> L
+dmeasure(po,x=x,y=y,times=t,params=theta,log=T) -> ll
+stopifnot(all.equal(ll[,1:3],log(L[,1:3])))
