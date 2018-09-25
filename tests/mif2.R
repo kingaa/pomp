56b8828edf63433fc3b5458c599de03bf1fa8a72
Aaron A. King <kingaa@umich.edu>
2018-09-25 08:37:20 -0400

    do away with fancy quotes in test results

--- tests/mif2.R
+++ tests/mif2.R
@@ -88,7 +88,8 @@ try(mif2(po,Nmif=2,Np=100,rw.sd=rw.sd(sigma=0.01,X_0=ivp(0.01)),
   cooling.fraction.50=0.1,dmeasure=function(log,...)stop("blop")))
 try(mif2(po,Nmif=2,Np=100,rw.sd=rw.sd(sigma=0.01,X_0=ivp(0.01)),
   cooling.fraction.50=0.1,dmeasure=function(log,...)NA))
-mif2(po,Nmif=2,Np=50,rw.sd=rw.sd(sigma=0.01),cooling.fraction.50=0.1,
+mif2(po,Nmif=2,Np=50,rw.sd=rw.sd(sigma=0.01),
+  cooling.type="hyper",cooling.fraction.50=0.1,
   drpocess="oops",
   dmeasure=function(log,...)0) -> mf3
 try(mif2(mf3,max.fail=1))
