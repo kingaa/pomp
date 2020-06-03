c5aa8db7d2920a5bd3ed01b59f66de088038ec09
Aaron A. King <kingaa@umich.edu>
2020-06-03 12:28:23 -0400

    remove untimely mention of 'wpfilter'; update tests

--- R/aaa.R
+++ R/aaa.R
@@ -3,13 +3,8 @@
 .onAttach <- function (...) {
   packageStartupMessage(
     "Welcome to pomp!\n",
-    "As of version 2.7.1.0, important changes have been made to the\n",
-    "default settings of the particle filtering algorithms in\n",
-    paste(sapply(c("pfilter","mif2","pmcmc","bsmc2"),sQuote),collapse=", "),".\n",
-    "These changes are not backward compatible.\n",
-    "See the package NEWS for the details.\n\n",
-    "For information on upgrading your pomp version < 2 code, see the\n",
-    dQuote("pomp version 2 upgrade guide")," at ",
-    "https://kingaa.github.io/pomp/",".\n"
+    "Version 3 incorporates some changes to the behavior of\n",
+    "package algorithms that are not backward compatible.\n",
+    "See the package NEWS for the details.\n\n"
   )
 }
