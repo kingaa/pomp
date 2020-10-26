90d0cd4785649ce208ad13d3e64228c5b579510f
Aaron A. King <kingaa@umich.edu>
2020-10-25 22:35:21 -0400

    minor change

--- R/bsplines.R
+++ R/bsplines.R
@@ -65,9 +65,7 @@ bspline.basis <- function (x, nbasis, degree = 3, deriv = 0, names = NULL) {
     pWarn(ep,"returning 0 since ",sQuote("deriv")," > ",sQuote("degree"))
   if (!is.null(names)) {
     if (length(names)==1) {
-      if (!grepl("%",names)) {
-        names <- paste0(names,".%d")
-      }
+      if (!grepl("%d",names)) names <- paste0(names,".%d")
       colnames(y) <- sprintf(names,seq_len(nbasis))
     } else if (length(names)==nbasis) {
       colnames(y) <- names
@@ -93,9 +91,7 @@ periodic.bspline.basis <- function (x, nbasis, degree = 3, period = 1,
     pWarn(ep,"returning 0 since ",sQuote("deriv")," > ",sQuote("degree"))
   if (!is.null(names)) {
     if (length(names)==1) {
-      if (!grepl("%",names)) {
-        names <- paste0(names,".%d")
-      }
+      if (!grepl("%d",names)) names <- paste0(names,".%d")
       colnames(y) <- sprintf(names,seq_len(nbasis))
     } else if (length(names)==nbasis) {
       colnames(y) <- names
