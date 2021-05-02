38e7507b15191de317bc0641bf8890236dd28e3f
Aaron A. King <kingaa@umich.edu>
2021-05-02 09:14:19 -0400

    update tests

--- tests/rgammawn.R
+++ tests/rgammawn.R
@@ -1,4 +1,15 @@
 library(pomp)
 set.seed(39596886L)
-rgammawn(5,sigma=1,dt=0.1)
-rgammawn(5,sigma=0.1,dt=0.1)
+
+stopifnot(
+  `m1 fail`=rgammawn(n=10000,sigma=1,dt=0.1) %>%
+    mean() %>%
+    all.equal(0.1,tolerance=0.05),
+  `m2 fail`=rgammawn(n=10000,sigma=0.1,dt=0.1) %>%
+    mean() %>%
+    all.equal(0.1,tolerance=0.05),
+  `v1 fail`=rgammawn(n=10000,sigma=1,dt=0.1) %>%
+    var() %>%
+    all.equal(0.1,tolerance=0.05)
+)
+
