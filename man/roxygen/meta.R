e91f4470280db13c4a8b2415f1fe58b121d20a76
Aaron A. King <kingaa@umich.edu>
2021-08-20 10:11:36 -0400

    improvements, change CITATION file

--- man/roxygen/meta.R
+++ man/roxygen/meta.R
@@ -1,7 +1,7 @@
 list(
   rd_family_title = list(
     `methods based on maximization`="More on estimation methods based on maximization:",
-    `full-information methods`="More on likelihood-based methods in \\pkg{pomp}:",
+    `full-information methods`="More on full-information (i.e., likelihood-based) methods in \\pkg{pomp}:",
     `Bayesian methods`="More on Bayesian methods in \\pkg{pomp}:",
     `MCMC methods`="More on \\pkg{pomp} methods based on Markov chain Monte Carlo:",
     `estimation methods`="More on \\pkg{pomp} estimation algorithms:",
@@ -10,7 +10,7 @@ list(
     `implementation information`="More on implementing POMP models: ",
     `pomp datasets`="More data sets provided with \\pkg{pomp}: ",
     `pomp examples`="More examples provided with \\pkg{pomp}: ",
-    `particle filter methods`="More on particle-filter based methods in \\pkg{pomp}: ",
+    `particle filter methods`="More on sequential Monte Carlo methods in \\pkg{pomp}: ",
     `summary statistic-based methods`="More on \\pkg{pomp} methods based on summary statistics: ",
     `deterministic methods`="More on \\pkg{pomp} methods for deterministic process models: "
   )
