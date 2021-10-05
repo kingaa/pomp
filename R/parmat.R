c0fa6c76af91fed3dd8cfcbe2b041149e8df9368
Aaron A. King <kingaa@umich.edu>
2021-10-05 07:02:37 -0400

    new 'names' argument for 'parmat'

--- R/parmat.R
+++ R/parmat.R
@@ -6,21 +6,22 @@
 ##'
 ##' @param params named numeric vector or matrix of parameters.
 ##' @param nrep number of replicates (columns) desired.
+##' @param names optional character; column names.
 ##' @return \code{parmat} returns a matrix consisting of \code{nrep} copies of
 ##' \code{params}.
 ##' @author Aaron A. King
 ##' @example examples/ricker-bifdiag.R
 ##' @export
-parmat <- function (params, nrep = 1) {
+parmat <- function (params, nrep = 1, names = NULL) {
   d <- dim(params)
   if (is.null(d) || length(d) == 1) {
     matrix(data=params,nrow=length(params),ncol=nrep,
-      dimnames=list(variable=names(params),rep=NULL))
+      dimnames=list(variable=names(params),rep=names))
   } else if (length(d) == 2) {
     matrix(data=params,nrow=nrow(params),ncol=ncol(params)*nrep,
-      dimnames=list(variable=rownames(params),rep=NULL))
+      dimnames=list(variable=rownames(params),rep=names))
   } else {
     matrix(data=params,nrow=nrow(params),ncol=prod(d[-1])*nrep,
-      dimnames=list(variable=rownames(params),rep=NULL))
+      dimnames=list(variable=rownames(params),rep=names))
   }
 }
--- tests/parmat.R
+++ tests/parmat.R
@@ -7,8 +7,19 @@ p
 p["b",] <- 1:3
 p <- parmat(p,2)
 p
-theta <- array(1:30,dim=c(5,3,2),dimnames=list(head(letters,5),NULL,NULL))
+theta <- array(
+  1:30,dim=c(5,3,2),
+  dimnames=list(head(letters,5),head(LETTERS,3),NULL)
+)
 p <- parmat(theta,2)
 p
-theta <- array(1:30,dim=c(5,3,2,1,1,1),dimnames=list(head(letters,5),NULL,NULL))
-stopifnot(all.equal(p,parmat(theta,2)))
+theta <- array(
+  1:30,
+  dim=c(5,3,2,1,1,1),
+  dimnames=list(head(letters,5),head(LETTERS,3),NULL)
+)
+q <- parmat(theta,2,names=head(LETTERS,12))
+stopifnot(
+  all.equal(p,parmat(theta,2)),
+  p==q
+)
