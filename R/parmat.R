419ca250995ca452bb3193ca1417677e40846bd0
Aaron A. King <kingaa@umich.edu>
2021-10-12 17:53:30 -0400

    - refactor 'parmat' as an S3 method - create 'parmat' method for data frames of parameters

--- R/parmat.R
+++ R/parmat.R
@@ -2,26 +2,120 @@
 ##'
 ##' \code{parmat} is a utility that makes a vector of parameters suitable for
 ##' use in \pkg{pomp} functions.
+##' @aliases parmat parmat,missing-method parmat,ANY-method
+##' @author Aaron A. King
 ##'
-##'
+setGeneric(
+  "parmat",
+  function (params, ...)
+    standardGeneric("parmat")
+)
+
+setMethod(
+  "parmat",
+  signature=signature(params="missing"),
+  definition=function (...) {
+    reqd_arg("parmat","params")
+  }
+)
+
+setMethod(
+  "parmat",
+  signature=signature(params="ANY"),
+  definition=function (params, ...) {
+    undef_method("parmat",params)
+  }
+)
+
 ##' @param params named numeric vector or matrix of parameters.
 ##' @param nrep number of replicates (columns) desired.
 ##' @param names optional character; column names.
+##' @param ... additional arguments, currently ignored.
 ##' @return \code{parmat} returns a matrix consisting of \code{nrep} copies of
 ##' \code{params}.
-##' @author Aaron A. King
 ##' @example examples/ricker-bifdiag.R
+##' 
+##' @rdname parmat
 ##' @export
-parmat <- function (params, nrep = 1, names = NULL) {
-  d <- dim(params)
-  if (is.null(d) || length(d) == 1) {
+setMethod(
+  "parmat",
+  signature=signature(params="numeric"),
+  definition=function (params, nrep = 1, ..., names = NULL) {
     matrix(data=params,nrow=length(params),ncol=nrep,
       dimnames=list(variable=names(params),.id=names))
-  } else if (length(d) == 2) {
-    matrix(data=params,nrow=nrow(params),ncol=ncol(params)*nrep,
-      dimnames=list(variable=rownames(params),.id=names))
-  } else {
-    matrix(data=params,nrow=nrow(params),ncol=prod(d[-1])*nrep,
-      dimnames=list(variable=rownames(params),.id=names))
   }
-}
+)
+
+##' @rdname parmat
+##' @export
+setMethod(
+  "parmat",
+  signature=signature(params="array"),
+  definition=function (params, nrep = 1, ..., names = NULL) {
+    d <- dim(params)
+    tryCatch(
+      if (length(d) == 1L) {
+        matrix(
+          data=as.numeric(params),
+          nrow=length(params),
+          ncol=nrep,
+          dimnames=list(
+            variable=names(params),
+            .id=names
+          )
+        )
+      } else if (length(d) == 2L) {
+        matrix(
+          data=as.numeric(params),
+          nrow=nrow(params),
+          ncol=ncol(params)*nrep,
+          dimnames=list(
+            variable=rownames(params),
+            .id=names
+          )
+        )
+      } else {
+        matrix(
+          data=as.numeric(params),
+          nrow=nrow(params),
+          ncol=prod(d[-1L])*nrep,
+          dimnames=list(
+            variable=rownames(params),
+            .id=names
+          )
+        )
+      },
+      warning = function (e) pStop("parmat",conditionMessage(e)),
+      error = function (e) pStop("parmat",conditionMessage(e))
+    )
+  }
+)
+
+##' @rdname parmat
+##' @export
+setMethod(
+  "parmat",
+  signature=signature(params="data.frame"),
+  definition=function (params, nrep = 1, ...) {
+    d <- dim(params)
+    rn <- rownames(params)
+    nrep <- as.integer(nrep)
+    if (nrep > 1)
+      rn <- as.character(outer(rn,seq_len(nrep),paste,sep="_"))
+    rv <- array(
+      data=NA_real_,
+      dim=c(d[2L],nrep*d[1L]),
+      dimnames=list(variable=names(params),.id=rn)
+    )
+    tryCatch(
+      for (n in names(params)) {
+        if (!is.numeric(params[[n]]))
+          pStop_(sQuote("params")," must contain numeric variables only.")
+        rv[n,] <- rep(as.double(params[[n]]),times=nrep)
+      },
+      warning = function (e) pStop("parmat",conditionMessage(e)),
+      error = function (e) pStop("parmat",conditionMessage(e))
+    )
+    rv
+  }
+)
--- tests/parmat.R
+++ tests/parmat.R
@@ -23,3 +23,35 @@ stopifnot(
   all.equal(p,parmat(theta,2)),
   p==q
 )
+
+try(parmat())
+try(parmat("bob"))
+
+theta <- c(a=1,b=3,c=4,d=5)
+Theta <- array(theta,dim=length(theta),dimnames=list(names(theta)))
+p1 <- parmat(Theta,nrep=2,names=c("A","B"))
+Theta <- array(theta,dim=c(length(theta),1),
+  dimnames=list(names(theta),NULL))
+p2 <- parmat(Theta,nrep=2,names=c("A","B"))
+Theta <- array(theta,dim=c(length(theta),1,1,1),
+  dimnames=list(names(theta),NULL,NULL,NULL))
+p3 <- parmat(Theta,nrep=2,names=c("A","B"))
+stopifnot(
+  identical(p1,p2),
+  identical(p1,p3)
+)
+
+try({
+  theta <- c(a="tom",b=3,c=4,d=5)
+  Theta <- array(theta,dim=length(theta),dimnames=list(names(theta)))
+  parmat(Theta,nrep=2,names=c("A","B"))
+})
+
+try(
+  expand.grid(a=1:3,c="hello",stringsAsFactors=FALSE) %>% parmat()
+)
+try(
+  expand.grid(a=1:3,c="hello",stringsAsFactors=TRUE) %>% parmat()
+)
+expand.grid(a=1:3,b=1:2) %>% parmat()
+expand.grid(a=1:3,b=1:2) %>% parmat(nrep=2)
