fbf3161a90d41cb5909f01039c04dbace75993ae
Aaron A. King <kingaa@umich.edu>
2022-12-29 12:22:36 -0500

    logmeanexp can compute ESS

--- R/logmeanexp.R
+++ R/logmeanexp.R
@@ -6,21 +6,26 @@
 ##'
 ##' When \code{se = TRUE}, \code{logmeanexp} uses a jackknife estimate of the
 ##' variance in \eqn{log(x)}.
+##' 
+##' When \code{ess = TRUE}, \code{logmeanexp} returns an estimate of the effective sample size.
 ##'
 ##' @importFrom stats sd
 ##' @param x numeric
 ##' @param se logical; give approximate standard error?
+##' @param ess logical; give effective sample size?
 ##' @return
-##' \code{log(mean(exp(x)))} computed so as to avoid over- or
-##' underflow.  If \code{se = FALSE}, the approximate standard error is
-##' returned as well.
+##' \code{log(mean(exp(x)))} computed so as to avoid over- or underflow.
+##' If \code{se = TRUE}, the approximate standard error is returned as well.
+##' If \code{ess = TRUE}, the effective sample size is returned also.
 ##' @author Aaron A. King
 ##' @example examples/logmeanexp.R
 ##' @export
-logmeanexp <- function (x, se = FALSE) {
+logmeanexp <- function (x, se = FALSE, ess = FALSE) {
   se <- isTRUE(se)
+  ess <- isTRUE(ess)
   mx <- max(x)
-  mean <- mx+log(mean(exp(x-mx)))
+  w <- exp(x-mx)
+  rv <- mx+log(mean(w))
   if (se) {
     n <- length(x)
     jk <- vapply(
@@ -28,8 +33,10 @@ logmeanexp <- function (x, se = FALSE) {
       \(k) logmeanexp(x[-k]),
       numeric(1L)
     )
-    c(mean,se=(n-1)*sd(jk)/sqrt(n))
-  } else {
-    mean
+    rv <- c(rv,se=(n-1)*sd(jk)/sqrt(n))
   }
+  if (ess) {
+    rv <- c(rv,ess=sum(w)^2/sum(w^2))
+  }
+  rv
 }
--- tests/logmeanexp.R
+++ tests/logmeanexp.R
@@ -1,4 +1,3 @@
-options(digits=7)
 library(pomp)
 
 logmeanexp(rep(10,10),se=TRUE)
@@ -7,4 +6,6 @@ logmeanexp(c(1.2e-100,1.4e-100,1.8e-100),se=TRUE)
 logmeanexp(c(1e-200,1.5e-200,1.6e-200))
 logmeanexp(10^seq(8,10),se=NA)
 logmeanexp(10^seq(8,10),se=NULL)
-
+logmeanexp(rep(10,10),ess=TRUE)
+logmeanexp(10^seq(8,10),ess=TRUE)
+logmeanexp(c(1.2e-100,1.4e-100,1.8e-100),se=TRUE,ess=TRUE)
