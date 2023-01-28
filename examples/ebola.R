c1486850ba5bd002b23aab7e3e5ea4b7fe65a75b
Aaron A. King <kingaa@umich.edu>
2023-01-28 09:16:28 -0500

    simplify ebola example

--- examples/ebola.R
+++ examples/ebola.R
@@ -1,24 +1,15 @@
 if (require(ggplot2) && require(tidyr)) {
 
-  data(ebolaWA2014)
-
   ebolaWA2014 |>
-    gather(variable,count,cases,deaths) |>
-    ggplot(aes(x=date,y=count,group=country,color=country))+
-    geom_line()+
-    facet_grid(variable~.,scales="free_y")+
-    theme_bw()+
-    theme(axis.text=element_text(angle=-90))
-
-  ebolaWA2014 |>
-    gather(variable,count,cases,deaths) |>
-    ggplot(aes(x=date,y=count,group=variable,color=variable))+
+    pivot_longer(c(cases,deaths)) |>
+    ggplot(aes(x=date,y=value,group=name,color=name))+
     geom_line()+
     facet_grid(country~.,scales="free_y")+
     theme_bw()+
     theme(axis.text=element_text(angle=-90))
 
-  plot(ebolaModel(country="SLE"))
-  plot(ebolaModel(country="LBR"))
-  plot(ebolaModel(country="GIN"))
 }
+
+plot(ebolaModel(country="SLE"))
+plot(ebolaModel(country="GIN"))
+plot(ebolaModel(country="LBR"))
