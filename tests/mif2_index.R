5202570bdf3b3fa79469249d2f2a293f5a6c7fc4
Aaron A. King <kingaa@umich.edu>
2024-12-09 08:36:33 -0500

    improve testing

--- tests/mif2_index.R
+++ tests/mif2_index.R
@@ -7,10 +7,12 @@ gompertz() -> gompertz
 set.seed(1481104436)
 
 gompertz |>
-  mif2(Nmif=4,Np=1000,
+  mif2(
+    Nmif=4,Np=1000,
     .indices=seq.int(1000),
     rw.sd=rw_sd(r=0.02,K=0.02,sigma=0.02),
-    cooling.fraction.50=0.5) |>
+    cooling.fraction.50=0.5
+  ) |>
   slot("indices") -> idx
 stopifnot(
   length(idx)==1000,
@@ -20,16 +22,24 @@ stopifnot(
 set.seed(962724905)
 
 gompertz |>
-  mif2(Nmif=4,Np=100,
+  mif2(
+    Nmif=4,Np=100,
     .indices=as.list(seq.int(100)),
     rw.sd=rw_sd(r=0.02,K=0.02,sigma=0.02),
-    cooling.fraction.50=0.5) |>
+    cooling.fraction.50=0.5
+  ) |>
   slot("indices") -> idx
 stopifnot(
   length(idx)==100,
   class(idx)=="list"
 )
 
-try(mif2(gompertz,Nmif=1,Np=100,
-  .indices=1:5,rw.sd=rw_sd(r=0.02,K=0.02,sigma=0.02),
-  cooling.fraction=0.5))
+try(
+  mif2(
+    gompertz,
+    Nmif=1,Np=100,
+    .indices=1:5,
+    rw.sd=rw_sd(r=0.02,K=0.02,sigma=0.02),
+    cooling.fraction.50=0.5
+  )
+)
