a5fec2ae7959d292be2717db6d1858c50715aa02
Aaron A. King <kingaa@umich.edu>
2026-08-18 07:27:53 -0400

    update roxygen, tests

--- tests/nullcases.R
+++ tests/nullcases.R
@@ -1,19 +1,34 @@
 library(pomp)
 
-simulate(times=1:10,t0=0,
-         statenames="x",
-         obsnames="y",
-         params=c(x_0=0),
-         rprocess=onestep(function(...)c(x=1)),
-         rmeasure=Csnippet("")
+simulate(
+  t0=0,
+  times=1:10,
+  statenames="x",
+  obsnames="y",
+  params=c(x_0=0),
+  rprocess=onestep(function(...)c(x=1)),
+  rmeasure=Csnippet("")
 ) -> s1
-states(s1)
-obs(s1)
+x <- states(s1)
+y <- obs(s1)
+stopifnot(
+  all(x==1),
+  all(is.na(y)),
+  dim(x)==c(1,10),
+  dim(y)==c(1,10)
+)
 
-simulate(times=1:10,t0=0,
-         statenames="x",
-         params=c(x_0=0),
-         rprocess=onestep(function(...)c(x=1))
+simulate(
+  t0=0,
+  times=1:10,
+  statenames="x",
+  params=c(x_0=0),
+  rprocess=onestep(function(...)c(x=1))
 ) -> s2
-states(s2)
-obs(s2)
+x <- states(s2)
+y <- obs(s2)
+stopifnot(
+  all(x==1),
+  dim(x)==c(1,10),
+  dim(y)==c(0,10)
+)
